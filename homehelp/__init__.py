3baab52162a6ef246195aa6e4500dc44239d8523
MAC <user@Adeyemi-Dev.local>
2025-06-18 08:36:21 -0700

    Handled missing config.py for deployment

--- homehelp/__init__.py
+++ homehelp/__init__.py
@@ -16,7 +16,11 @@ def create_app():
     app = Flask(__name__,instance_relative_config=True)
 
     #to load all the config inside the file
-    app.config.from_pyfile("config.py")
+    try:
+        app.config.from_pyfile("config.py")
+    except FileNotFoundError:
+        pass
+
     app.config.from_object(config.BaseConfig)
 
     #db = SQLAlchemy(app)
