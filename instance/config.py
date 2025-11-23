a3a9db8a9d1a7268317bf053ea420d2f06bee46d
Raya Codes <adeyemiid1@gmail.com>
2025-11-23 12:24:37 +0100

    Fixed merge conflict

---
+++ instance/config.py
@@ -0,0 +1,8 @@
+SECRET_KEY = '9Pkq_EJ2KEY'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root@localhost/homehelp_db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+MAIL_SERVER = 'smtp.gmail.com' 
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'adeyemiid1@gmail.com'
+MAIL_PASSWORD = 'your-password'
